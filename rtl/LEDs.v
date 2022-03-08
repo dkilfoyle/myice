a0668cc44757677e50f1ed8aa8f7bd3d40acd56c
Dean Kilfoyle <dkilfoyle@xtra.co.nz>
2022-03-08 23:26:49 +1300

    verilog compiles

---
+++ rtl/LEDs.v
@@ -0,0 +1,34 @@
+// femtorv32, a minimalistic RISC-V RV32I core
+//       Bruno Levy, 2020-2021
+//
+// This file: driver for LEDs (does nearly nothing !)
+//
+
+module LEDDriver(
+    input wire 	       clk, // system clock
+    input wire 	       rstrb, // read strobe		
+    input wire 	       wstrb, // write strobe
+    input wire 	       sel, // select (read/write ignored if low)
+    input wire [31:0]  wdata, // data to be written
+    output wire [31:0] rdata, // read data
+    output wire [3:0]  LED    // LED pins
+);
+
+   reg [3:0] led_state;
+   assign LED = led_state;
+   
+   initial begin
+      led_state = 4'b0000;
+   end
+   
+   assign rdata = (sel ? {28'b0, led_state} : 32'b0);
+   
+   always @(posedge clk) begin
+      if(sel && wstrb) begin
+	 led_state <= wdata[3:0];	 
+`ifdef BENCH
+         $display("****************** LEDs = %b", wdata[3:0]);
+`endif	 
+      end
+   end
+endmodule
